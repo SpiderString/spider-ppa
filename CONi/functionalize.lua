80def5472544e2fd125e1337ae78fdcd202ec934
SpiderString <mailspam124@gmail.com>
2020-04-26 02:57:11 -0500

    Added support for external scripts. Renamed several script functions to include "Embedded" in the name for clarity. General refactoring.

--- CONi/functionalize.lua
+++ CONi/functionalize.lua
@@ -39,7 +39,7 @@ function lib.getScript(con, index)
   end
   return output
 end
-local function runScript(script)
+local function runEmbeddedScript(script)
   local filename="tmp"
   local num=1
   while filesystem.exists(filename..tostring(num)..".lua") do
@@ -59,12 +59,12 @@ local function getScriptName(line)
   if not lib.isScript(line) then return nil end
   return line[2]:sub(2):gsub("^[%s]", "")
 end
-local function executeScript(con, index)
+local function executeEmbeddedScript(con, index)
   local script=lib.getScript(con, index)
   if not script then return nil end
-  return runScript(script)
+  return runEmbeddedScript(script)
 end
-function lib.executeScripts(con)
+function lib.executeEmbeddedScripts(con)
   local output={}
   local script
   local scriptName
@@ -72,7 +72,7 @@ function lib.executeScripts(con)
   for i=1, #con do
     line=con[i]
     if lib.isScript(line) then
-      script=executeScript(con, i)
+      script=executeEmbeddedScript(con, i)
       scriptName=getScriptName(line)
       output[scriptName]=script
     end
@@ -86,14 +86,13 @@ function lib.isFunc(line)
   if not line or type(line)~="table" then return false end
   if tokenLib.isContainer(line) and line[2]:find("^@") then return true end
 end
-local function getFunc(scripts, con, index)
+--returns {name, scriptName, depth, script, ...}
+local function getFunc(con, index)
   local func={}
   local line = con[index]
   local props
-  local funcEnd
-  local str
   local funcName
-  if not scripts or not lib.isFunc(line) then return nil end
+  if not lib.isFunc(line) then return nil end
   func.depth=line[1]
   func.line=index
   funcName=line[2]:sub(2):gsub("^[%s]", "")
@@ -104,23 +103,33 @@ local function getFunc(scripts, con, index)
       func[line[2]] = concatTable(line[4]):gsub("\"", "")
     end
   end
-  if not func.script then return nil end
+  if not func.script then return nil end --required prop
   func.name=funcName
   func.scriptName=func.func or func.name
-  if not scripts[func.script] then return nil end --script not found error
-  func.func=scripts[func.script][func.scriptName]
   return func
 end
 function lib.getFuncs(con)
-  local scripts = lib.executeScripts(con)
+  local scripts=lib.executeEmbeddedScripts(con)
   local funcs={}
   local line
-  if not scripts then return nil end
+  --get func declarations
   for i=1, #con do
     line=con[i]
     if lib.isFunc(line) then
-      table.insert(funcs, getFunc(scripts, con, i))
+      table.insert(funcs, getFunc(con, i))
     end
+  end
+  --get external scripts and bind functions
+  for i, f in ipairs(funcs) do
+    if not scripts[f.script] then
+      if filesystem.exists(f.script) then
+        scripts[f.script]=run(f.script)
+      else
+        --Script doesn't exist. Could choose to error here or warn.
+        funcs[i]=nil; f=nil
+      end
+    end
+    f.func=f and scripts[f.script][f.scriptName]
   end
   return funcs
 end
