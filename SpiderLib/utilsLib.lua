da9845ef1b67f1b3a3868fd6ff5fe05c396e1561
SpiderString <mailspam124@gmail.com>
2021-04-20 15:04:16 -0500

    Refactored combinations. Removed goto statements. Needs review.

--- SpiderLib/utilsLib.lua
+++ SpiderLib/utilsLib.lua
@@ -106,50 +106,57 @@ function lib.contains(v, t)
   return false
 end
 function lib.combinations(n, _t)
-  local function push(k, a, b)
-    for i=1, math.min(k, #a) do
-      table.insert(b, 1, table.remove(a, 1))
+  local function popPush(k, src, dest)
+    for i=1, math.min(k, #src) do
+      table.insert(dest, 1, table.remove(src, 1))
     end
   end
+
   local t = {table.unpack(_t)}
   local output={}
   local build={}
-  local c={} --buffer1
-  local d={} --buffer2 "protected buffer"
-  ::Recur::
-  if #t < n then return output end
-  push(n-1, t, build)
-  ::Loop::
-  if #t == 0 then --fully exhausted combinations with current head
-    push(#build-1, build, t)
-    push(#d, d, t)
-    build={}
-    goto Recur
-  end
-  for i=1, #t do
-    local elem={}
-    for i, e in ipairs(build) do --build is in stacked formation(reverse order)
-      table.insert(elem, 1, e)
+  local buffer1={}
+  local buffer2={} --"protected buffer"
+
+  while #t >= n do
+    popPush(n - 1, t, build)
+
+    while true do
+      if #t == 0 then --fully exhausted combinations with current head
+        popPush(#build-1, build, t)
+        popPush(#buffer2, buffer2, t)
+        build={}
+        break
+      end
+
+      for i=1, #t do
+        local elem={}
+        for i, e in ipairs(build) do --build is in stacked formation(reverse order)
+          table.insert(elem, 1, e)
+        end
+        table.insert(elem, t[i])
+        table.insert(output, elem)
+      end
+
+      if #build==0 then --n=1
+        return output
+      elseif #build == 1 then --n=2 or certain combinations of (n, k)
+        build={}
+        break
+      end
+
+      popPush(1, build, buffer1)
+      popPush(1, t, build)
+      if #t==0 then --exhausted combinations with current init
+        popPush(1, build, t)
+        popPush(#buffer1, buffer1, t)
+        if #build==1 then recur() end --init=head, discard head
+        popPush(1, build, buffer2)
+        popPush(n-1-#build, t, build)
+      end
     end
-    table.insert(elem, t[i])
-    table.insert(output, elem)
   end
-  if #build==0 then --n=1
-    return output
-  elseif #build == 1 then --n=2 or certain combinations of (n, k)
-    build={}
-    goto Recur
-  end
-  push(1, build, c)
-  push(1, t, build)
-  if #t==0 then --exhausted combinations with current init
-    push(1, build, t)
-    push(#c, c, t)
-    if #build==1 then recur() end --init=head, discard head
-    push(1, build, d)
-    push(n-1-#build, t, build)
-  end
-  goto Loop
+  return output
 end
 
 return lib
